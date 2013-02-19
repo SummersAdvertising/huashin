273b0defa781cca24d900fb12f0ca03cc120e327
HanaHsu <kobanae@gmail.com>
2013-02-19 18:07:34 +0800

    css修正

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,8 +1,9 @@
 class StaticPageController < ApplicationController
   def index
-  	layout 'index'
   	@news = News.order('created_at DESC').page(params[:page])
+  	render :template => 'layout/index'
   end
   def intro
+  	render :template => 'layout/application'
   end
 end
