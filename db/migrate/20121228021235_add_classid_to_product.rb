21fd0673580c3928890e59bf3689146984af52fd
HanaHsu <kobanae@gmail.com>
2013-02-19 14:51:31 +0800

    fix migration file

--- db/migrate/20121228021235_add_classid_to_product.rb
+++ db/migrate/20121228021235_add_classid_to_product.rb
@@ -1,5 +1,5 @@
 class AddClassidToProduct < ActiveRecord::Migration
   def change
-  	add_column :products, :productclass_id, :interger
+  	add_column :products, :productclass_id, :integer
   end
 end
