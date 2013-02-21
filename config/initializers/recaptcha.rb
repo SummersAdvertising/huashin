30028006f1a43018054476f66a80837cf012cb5d
HanaHsu <kobanae@gmail.com>
2013-02-21 14:38:15 +0800

    add recaptcha

--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6LdBNN0SAAAAAOZG0EmcgwNY2UMH46Yz1-OTgcpf'
-  config.private_key = '6LdBNN0SAAAAABNViMf_OLrQJZ5mf7zCYmIkLtKr'
-  #config.proxy = 'http://myproxy.com.au:8080'
-end
